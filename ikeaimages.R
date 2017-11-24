f22930cc2e2ddd4c927a8607ffbedeb213f9856e
longhowlam <longhowlam@gmail.com>
2017-11-24 20:14:39 +0000

    images

--- ikeaimages.R
+++ ikeaimages.R
@@ -6,12 +6,12 @@ library(stringr)
 vgg16_notop = application_vgg16(weights = 'imagenet', include_top = FALSE)
 
 
-### Some brad pitt images
+### Some ikea images
 i=1
 for(i in 1:5)
 {
   img = image_load(
-    paste0("images/bp0", i,".jpg"), target_size = c(224,224)
+    paste0("images/ikea", i,".PNG"), target_size = c(224,224)
   )
   x = image_to_array(img)
   
@@ -30,32 +30,4 @@ for(i in 1:5)
 }
 
 
-
-ff = list.files("images")
-IM = ff[str_detect(ff,glob2rx("IM*"))]
-
-for(i in 1:length(IM))
-{
-  img = image_load( paste0("images/", IM[i]), target_size = c(224,224))
-  x = image_to_array(img)
-  
-  dim(x) <- c(1, dim(x))
-  x = imagenet_preprocess_input(x)
-  
-  # extract features
-  features = vgg16_notop %>% predict(x)
-  f1 = as.numeric(features)
-  if( i==1 ){
-    M2 <- as(matrix(f1, ncol = length(f1)), "dgCMatrix")
-  }else{
-    M2 = rbind(M2, f1)
-  }
-}
-
-
-
-afstanden = text2vec::dist2(M2,M1)
-rowMeans(as.matrix(afstanden))
-
-saveRDS(M1, "bradmatrix.RDs")
 
---
+++ ikealinks.R
@@ -0,0 +1,23 @@
+ikeaimages = data.frame(
+  type = c(
+    "bank",
+    "bank",
+    "bank",
+    "kast",
+    "kast"
+  ),
+  image = c(
+    "ikea1",
+    "ikea2",
+    "ikea3",
+    "ikea4",
+    "ikea5"
+  ),
+  link = c(
+    "http://www.ikea.com/nl/nl/catalog/products/S89128236/",
+    "http://www.ikea.com/nl/nl/catalog/products/S89211400/",
+    "http://www.ikea.com/nl/nl/catalog/products/S79150459/",
+    "http://www.ikea.com/nl/nl/catalog/products/S79128661/",
+    "http://www.ikea.com/nl/nl/catalog/products/30311312/"
+  )
+)
