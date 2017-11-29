9d2022d9268dbc9feb7097aeae682bb8a28ff036
longhowlam <longhowlam@gmail.com>
2017-11-29 12:15:42 +0000

    ikea search functions

--- image_extraction.R
+++ image_extraction.R
@@ -1,28 +1,22 @@
 library(rvest)
 library(stringr)
 library(dplyr)
+library(futile.logger)
 
-#### twee persoons bedden #####################################################################
-link = "http://www.ikea.com/nl/nl/catalog/categories/departments/bedroom/16284/"
-out = read_html(link)
-zz = '//a[@class="productLink"]/@href'
-productLinks = html_nodes(out,xpath = zz) %>% html_text
-bedden = extractImage(productLinks, soort = "bedden")
+##############################################################################################
+## Op product categorieen
 
-#########  Bureaus ###########################################################################
-link = "http://www.ikea.com/nl/nl/catalog/categories/departments/workspaces/20649/"
-out = read_html(link)
-zz = '//a[@class="productLink"]/@href'
-productLinks = html_nodes(out,xpath = zz) %>% html_text
-bureaus = extractImage(productLinks, soort = "bureaus")
+#### twee persoons bedden ########
+mainlink = "http://www.ikea.com/nl/nl/catalog/categories/departments/bedroom/16284/"
+bedden = extractImage(mainlink)
 
-####### Boeken kasten #######################################################################
-link = "http://www.ikea.com/nl/nl/catalog/categories/departments/living_room/10382/"
-out = read_html(link)
-zz = '//a[@class="productLink"]/@href'
-productLinks = html_nodes(out,xpath = zz) %>% html_text
-boekenkasten = extractImage(productLinks, soort = "boekenkasten")
+#########  Bureaus ###############
+mainlink = "http://www.ikea.com/nl/nl/catalog/categories/departments/workspaces/20649/"
+bureaus = extractImage(mainlink)
 
+####### Boeken kasten ############
+mainlink = "http://www.ikea.com/nl/nl/catalog/categories/departments/living_room/10382/"
+boekenkasten = extractImage(mainlink)
 
 
 ######### combine ##################
@@ -31,16 +25,108 @@ allImages = bind_rows(bedden, bureaus, boekenkasten)
 saveRDS(allImages, "AllImages.RDs")
 
 
-##########  helper function ####################################################
 
-extractImage = function(href, soort)
+
+###############################################################################
+######  op zoekpaginas scrapen #########################
+
+stoeltjes = searchIkea("stoeltje")
+
+
+
+
+
+
+##########  helper functions ####################################################
+
+### function to retrive images from products that resulted on the IKEA search
+
+searchIkea = function(query){
+
+  #### first retrieve N pages on the first page 
+  baselink = paste0(
+    "http://www.ikea.com/nl/nl/search/?query=",
+    query ,
+    "&pageNumber="
+  )
+  firstpagelink = paste0(baselink , "1")
+  
+  flog.info("SEARCH %s", firstpagelink)
+  out = read_html(firstpagelink)
+  tmp =  html_nodes(out, xpath = '//a/@href') %>% html_text()
+  pages = tmp[stringr::str_detect(tmp, "pageNumber")]
+
+  NPAGES = pages %>% str_extract("\\d+") %>% as.numeric() %>% max()
+
+  flog.info("npages %s", NPAGES)
+  
+  flog.info("loop over pages")
+  #### loop over the search pages 
+  if(NPAGES > 0){
+  iter = 1:NPAGES
+  purrr::map_df(iter, searchIkeaOnePage, baselink, query)
+  }
+}
+
+
+searchIkeaOnePage = function(iter, baselink, query)
 {
+  link = paste0(baselink,iter)
+  out = read_html(link)
+  
+  lijst =  html_nodes(out, xpath = '//a/@href') %>% html_text()
+  producten = str_detect(lijst , "catalog/products")
+  bvtab = str_detect(lijst , "bvtab")
+  geldigeproductlinks = lijst[producten & !bvtab]
+  
+  outframe = data.frame()
+  for( j in 1:length(geldigeproductlinks))
+  {
+    link = paste0("http://www.ikea.com/", geldigeproductlinks[j])
+    out = read_html(link)
+    type = query
+    
+    zz = 
+    img = html_nodes(
+      out,
+      xpath = '//img[@id="productImg"]/@src'
+    ) %>% 
+      html_text()
+    
+    imagefile = str_sub(img,24,str_length(img))
+    
+    download.file(
+      paste0("http://www.ikea.com", img),
+      destfile = paste0("images/", imagefile),
+      quiet = TRUE
+    )
+    
+    outframe = bind_rows(
+      outframe, 
+      data.frame(link,type,imagefile, stringsAsFactors = FALSE)
+    )
+  }
+  flog.info("search page %s processed", iter)
+  outframe
+}
+
+
+
+
+#### given a main product page, extract images ##########
+
+extractImage = function(mainlink)
+{
+  out = read_html(mainlink)
+  zz = '//a[@class="productLink"]/@href'
+  href = html_nodes(out,xpath = zz) %>% html_text
+  
   outframe = data.frame()
   for( i in 1:length(href))
   {
     link = paste0("http://www.ikea.com/", href[i])
     out = read_html(link)
-    type = soort
+    type = mainlink
   
     zz = '//img[@id="productImg"]/@src'
     img = html_nodes(out,xpath = zz) %>% html_text()
@@ -50,7 +136,10 @@ extractImage = function(href, soort)
       destfile = paste0("images/", imagefile)
     )
   
-    outframe = bind_rows(outframe, data.frame(link,type,imagefile))
+    outframe = bind_rows(
+      outframe, 
+      data.frame(link,type,imagefile, stringsAsFactors = FALSE)
+    )
   }
   outframe
 }
